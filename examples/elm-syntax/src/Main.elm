490107173f3fe5a514303622c61f9827728cf216
Jordy Moos <jordymoos@gmail.com>
2019-05-14 22:46:45 +0200

    Working on the talk

--- examples/elm-syntax/src/Main.elm
+++ examples/elm-syntax/src/Main.elm
@@ -15,12 +15,15 @@ isEqual =
 addFive : number -> number
 addFive = add 5
 
-
+addFiveIsEqual : Bool
+addFiveIsEqual =
+    addFive 10 == add 5 10
 
 
 main : Bool
 main =
     let
         _ = addFive 10
+        _ = addFiveIsEqual
     in
         True
